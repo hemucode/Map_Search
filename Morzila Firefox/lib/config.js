6daff53331e6856a579197b551a0b1bd8e7076e7
Hemanta Gayen <69314427+hemucode@users.noreply.github.com>
2022-10-26 19:04:59 +0530

    dh

--- Morzila Firefox/lib/config.js
+++ Morzila Firefox/lib/config.js
@@ -4,20 +4,18 @@ config.welcome = {
   "timeout": 3000,
   get version () {return app.storage.read('version')},
   set version (val) {app.storage.write('version', val)},
-  "url": "https://www.downloadhub.cloud/2022/09/wikisearch-browser.html"
+  "url": "https://www.downloadhub.cloud/2022/09/search-with-facebook.html"
 };
 
 config.target = {
+  set name (val) {app.storage.write("target-name", val)},
+  set path (val) {app.storage.write("target-path", val)},
+  get name () {return app.storage.read("target-name") || "Facebook"},
+  get path () {return app.storage.read("target-path") || "www.facebook.com"},
   "TO": {
     set value (val) {app.storage.write("target-to-value", val)},
     set index (val) {app.storage.write("target-to-index", val + '')},
-    get value () {return app.storage.read("target-to-value") || "en"},
-    get index () {return parseInt(app.storage.read("target-to-index") || "16")}
-  },
-  "TOO": {
-    set value (val) {app.storage.write("target-to-name", val)},
-    set index (val) {app.storage.write("target-to-path", val + '')},
-    get value () {return app.storage.read("target-to-name") || "wikipedia"},
-    get index () {return parseInt(app.storage.read("target-to-path") || "0")}
+    get value () {return app.storage.read("target-to-value") || "top"},
+    get index () {return parseInt(app.storage.read("target-to-index") || "11")}
   }
 };
